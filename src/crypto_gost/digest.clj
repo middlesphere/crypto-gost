aff2dd2f1e19c1613c3765f9eede8516bf89cc21
Mike Ananev <mike@middlesphere.com>
2017-07-03 21:30:57 +0300

    hmac added. some test improved.

--- src/crypto_gost/digest.clj
+++ src/crypto_gost/digest.clj
@@ -1,10 +1,13 @@
 (ns crypto-gost.digest
   (:gen-class)
-  (:require [clojure.java.io :as io :refer [input-stream]])
+  (:require [clojure.java.io :as io :refer [input-stream]]
+            [crypto-gost.common :as common])
   (:import java.security.Security
            [org.bouncycastle.crypto.digests GOST3411_2012_256Digest GOST3411_2012_512Digest GOST3411Digest]
-           org.bouncycastle.jce.provider.BouncyCastleProvider
-           org.bouncycastle.util.encoders.Hex))
+           org.bouncycastle.crypto.generators.PKCS5S1ParametersGenerator
+           org.bouncycastle.crypto.macs.HMac
+           org.bouncycastle.crypto.params.KeyParameter
+           org.bouncycastle.jce.provider.BouncyCastleProvider))
 
 (defn- digest-class
   "return initilized GOST digest class based on given algo-type"
@@ -32,7 +35,7 @@
   "calculate GOST digest from string.
   return digest as hex String"
   [algo-type s]
-  (Hex/toHexString (digest algo-type (.getBytes s))))
+  (common/bytes-to-hex (digest algo-type (.getBytes s))))
 
 
 (defn digest-stream
@@ -51,4 +54,41 @@
           (recur (do (.update digest buf 0 n) (.read in buf))))))))
 
 
+(defn hmac
+  "calculate GOST hmac from given byte array.
+  return hmac as bytes array"
+  [algo-type bytes-data ^String seed]
+  (Security/addProvider (BouncyCastleProvider.))
+  (let [hmac-fn (HMac. (digest-class algo-type))
+        key-param (KeyParameter. (PKCS5S1ParametersGenerator/PKCS5PasswordToUTF8Bytes (.toCharArray seed)))
+        _ (.init hmac-fn key-param)
+        _ (.update hmac-fn bytes-data 0 (alength bytes-data))
+        hmac-byte-array (byte-array (.getMacSize hmac-fn))
+        _ (.doFinal hmac-fn hmac-byte-array 0)]
+    hmac-byte-array))
+
+
+(defn hmac-str
+  "calculate GOST hmac from string using given seed.
+  return hmac as hex String"
+  [algo-type ^String s ^String seed]
+  (common/bytes-to-hex (hmac algo-type (.getBytes s) seed)))
+
+
+(defn hmac-stream
+  "calculate GOST hmac for given streaming input.
+  As input may be:  File, URI, URL, Socket, byte array, or filename as String  which  will be coerced to BufferedInputStream and auto closed after.
+  return hmac as byte array."
+  [algo-type input ^String seed]
+  (Security/addProvider (BouncyCastleProvider.))
+  (with-open [in (input-stream input)]
+    (let [buf (byte-array 1024)
+          hmac-fn (HMac. (digest-class algo-type))
+          key-param (KeyParameter. (PKCS5S1ParametersGenerator/PKCS5PasswordToUTF8Bytes (.toCharArray seed)))
+          _ (.init hmac-fn key-param)
+          hmac-buffer (byte-array (.getMacSize hmac-fn))]
+      (loop [n (.read in buf)]
+        (if (<= n 0)
+          (do (.doFinal hmac-fn hmac-buffer 0) hmac-buffer)
+          (recur (do (.update hmac-fn buf 0 n) (.read in buf))))))))
 
--- test/crypto_gost/digest_test.clj
+++ test/crypto_gost/digest_test.clj
@@ -27,6 +27,14 @@
 (def m3-3411-12-512 "275557a47dcfcb8235ff029b74837f0441efe41aed12c207313e83b27abd1e6a9d892713bc30a16bf947d46a59bbbb3a33ee33385391c73675e7d0c360213540")
 (def m4-3411-12-512 "eeb2c35b760457d290022fc060e29500122ccdbd73b834ec04048d6de75e942fc52df86fa0ddddfce882b8dbda573ffba0232903c4c057b76624962809c184bf")
 
+;; hmac constants (not from standard)
+(def const-seed "012346789abcdef")
+(def m3-hmac-3411-94 "3a05abb54a68dc8d22bb4b7d19999ebc10f2fbbcfe0167ceb0ba8fbb80e0250f")
+(def m3-hmac-3411-12-256 "a8b619bc2800e5875db0c8a5aeee636acad5fa628e729b1c8f0c4f600b729f07")
+(def m3-hmac-3411-12-512 "d6d4feaaf80fbcd54b7e0322492898efaee597c25b8a414ed8ebda4f380cf0d138dc13ab7d6b3451f6e67822198b5cf6dfd5bb392aba03b7521d51cd725c91bd")
+
+
+
 (deftest gost-3411-94-test
   (let [r1 (sut/digest-str :3411-94 m1)
         r2 (sut/digest-str :3411-94 m2)
@@ -59,13 +67,44 @@
     (is (= m3-3411-12-512 r3))
     (is (= m4-3411-12-512 r4))))
 
+
 (deftest gost-3411-all-streams
   (let [r1 (bytes-to-hex (sut/digest-stream :3411-94 "test/data/m1.txt"))
         r2 (bytes-to-hex (sut/digest-stream :3411-94 "test/data/m2.txt"))
         r3 (bytes-to-hex (sut/digest-stream :3411-94 "test/data/m3.txt"))
-        r4 (bytes-to-hex (sut/digest-stream :3411-94 "test/data/m4.txt"))]
+        r4 (bytes-to-hex (sut/digest-stream :3411-94 "test/data/m4.txt"))
+
+        r5 (bytes-to-hex (sut/digest-stream :3411-2012-256 "test/data/m1.txt"))
+        r6 (bytes-to-hex (sut/digest-stream :3411-2012-256 "test/data/m2.txt"))
+        r7 (bytes-to-hex (sut/digest-stream :3411-2012-256 "test/data/m3.txt"))
+        r8 (bytes-to-hex (sut/digest-stream :3411-2012-256 "test/data/m4.txt"))
+
+        r9 (bytes-to-hex (sut/digest-stream :3411-2012-512 "test/data/m1.txt"))
+        r10 (bytes-to-hex (sut/digest-stream :3411-2012-512 "test/data/m2.txt"))
+        r11 (bytes-to-hex (sut/digest-stream :3411-2012-512 "test/data/m3.txt"))
+        r12 (bytes-to-hex (sut/digest-stream :3411-2012-512 "test/data/m4.txt"))]
+
     (is (= m1-3411-94 r1))
     (is (= m2-3411-94 r2))
     (is (= m3-3411-94 r3))
-    (is (= m4-3411-94 r4))))
+    (is (= m4-3411-94 r4))
 
+    (is (= m1-3411-12-256 r5))
+    (is (= m2-3411-12-256 r6))
+    (is (= m3-3411-12-256 r7))
+    (is (= m4-3411-12-256 r8))
+
+    (is (= m1-3411-12-512 r9))
+    (is (= m2-3411-12-512 r10))
+    (is (= m3-3411-12-512 r11))
+    (is (= m4-3411-12-512 r12))))
+
+
+(deftest hmac-all
+  (let [r1 (bytes-to-hex (sut/hmac-stream :3411-94 "test/data/m3.txt" const-seed))
+        r2 (bytes-to-hex (sut/hmac-stream :3411-2012-256 "test/data/m3.txt" const-seed))
+        r3 (bytes-to-hex (sut/hmac-stream :3411-2012-512 "test/data/m3.txt" const-seed))]
+
+    (is (= r1 m3-hmac-3411-94))
+    (is (= r2 m3-hmac-3411-12-256))
+    (is (= r3 m3-hmac-3411-12-512))))
