5d8509fc10dc002b089d2e370e32ec967cd8ea89
Mike Ananev <mike@middlesphere.com>
2018-02-17 12:56:51 +0300

    Version 0.2.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject crypto-gost "0.2.3-SNAPSHOT"
+(defproject crypto-gost "0.2.3"
   :description "Clojure Bouncycastle wrapper to work with GOST"
   :url "https://github.com/middlesphere/crypto-gost"
   :author "Mike Ananev"
